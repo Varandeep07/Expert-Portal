0beb9f79c7585a95d38a5daa56e3e3fdbf9c57f5
Varandeep07 <varandeepsahota@gmail.com>
2023-04-29 15:56:08 +0530

    changes in pagination

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const CustomNavbar = () => {
   return (
     <Navbar className="navbar navbar-light shadow rounded-lg" style={{backgroundColor:"#eee7b2"}}>
       <Navbar.Brand href="/" style={{marginLeft: "10px"}}><bold>Expert Portal</bold></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
